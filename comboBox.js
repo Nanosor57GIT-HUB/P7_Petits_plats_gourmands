d9f5c48b787a45456eef44bc89570e0f8b0f4e34
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2022-06-27 18:05:46 +0200

    séparation function getIngredient

--- comboBox.js
+++ comboBox.js
@@ -7,6 +7,7 @@ let component = [];
 let ustensile = [];
 let appareils = [];
 
+//Récupération des datas api
 function getDataCombo() {
   //Récupération des tableaux ingrédients, appareils et ustensiles
   recipes.forEach((recipe) => {
@@ -15,52 +16,37 @@ function getDataCombo() {
 
     appareils.push(appliance);
     appliance = [...new Set(appareils)];
-    // localStorage.setItem("appareils", appliance);
-    //console.log(appliance);
-
+ 
+    //Récupération de chaque ustensiles/new array cookingTools
     ustens = recipe.ustensils;
 
-    //Récupération de chaque ustensiles/new array cookingTools
     ustens.forEach((ust) => {
       cookingTools = ust;
       ustensile.push(cookingTools);
       cookingTools = [...new Set(ustensile)];
-      // localStorage.setItem("ustensiles", cookingTools);
-      // console.log(cookingTools);
     });
+
+     //Récupération de chaque component/new Array ingredient
     ingredients = recipe.ingredients;
-    //Récupération de chaque component/new Array ingredient
+   
     ingredients.forEach((ing) => {
       ingredient = ing.ingredient;
       //Pousser les éléments dans le tableau component
       component.push(ingredient);
       //Suppression des doublons
       ingredient = [...new Set(component)];
-      // localStorage.setItem("ingredient", ingredient);
-      //  console.log(ingredient);
     });
   });
 }
 getDataCombo(component, ustensile, appareils);
 
-//Trier les noms par ordre croissant (native)
+//Trier les noms des listes par ordre croissant (native)
 let sortedIngredients = ingredient.sort();
 let sortedUstensile = cookingTools.sort();
 let sortedAppareils = appliance.sort();
-//console.log(sortedAppareils);
 
-function createItem(parent, listing) {
-  //Création d'un élément li
-  let listItem = document.createElement("li");
-  listItem.classList.add("list-items");
-  listItem.style.cursor = "pointer";
-  // listItem.setAttribute("onclick", "displayTags('" + listing + "')");
-  listItem.addEventListener("click", displayTags.bind(null, listing));
-  let word = listing;
-  //afficher la valeur dans le tableau
-  listItem.innerText = word;
-  parent.querySelector(".ulCombo").appendChild(listItem);
-}
+
+
 // parent.innerHTML = "";
 //regarder/compréhension
 //event.target
@@ -73,55 +59,73 @@ const combos = [
   { input: input3, list: sortedUstensile },
 ];
 
-
-//Réinitialisation du combo sur le click d'un autre combo
-function initCombo() {
-for (let combo of combos) {
-combo.input.addEventListener("click", (e) => {
-combos.forEach((close) => (close.input.value = ""));
-removeElements();
-})
+//Création des listes
+function createItem(parent, listing) {
+  //Création d'un élément li
+  let listItem = document.createElement("li");
+  listItem.classList.add("list-items");
+  listItem.style.cursor = "pointer";
+  listItem.addEventListener("click", displayTags.bind(null, listing));
+  let word = listing;
+  //afficher la valeur dans le tableau
+  listItem.innerText = word;
+  parent.querySelector(".ulCombo").appendChild(listItem);
+  
 }
-}
-
-
+/**************************************************************** */
 
 
 for (let combo of combos) {
-  combo.input.addEventListener("keyup", (e) => {
-   // arrow.classList.toggle("toggleArrow");
-    //    console.log(e.arrow);
- 
-    removeElements(parent);
+  
+  combo.input.addEventListener("keyup", () => {
+      removeList(parent);
+
     for (let listing of combo.list) {
       if (
         listing.toLowerCase().startsWith(combo.input.value.toLowerCase()) &&
         combo.input.value != ""
       ) {
         const parent = combo.input.closest("form");
+
         createItem(parent, listing);
-      }
+
+        combo.input.style.borderRadius = "5px 5px 0 0";
+      } 
     }
+    
   });
 }
 
-function removeElements() {
-  //effacer tous les items
-  let items = document.querySelectorAll(".list-items");
-  items.forEach((item) => {
-    item.remove();
-    initCombo();
-    combos.forEach(close => close.input.value = "") 
-  });
-}
 
-// arrow.classList.toggle("toggleArrow");
+//localStorage.clear();
 
 function displayTags(value) {
   let listTags = document.createElement("li");
   listTags.classList.add("list-tags");
   const tags = document.querySelector(".tags").appendChild(listTags);
-  listTags.innerText = value;
+listTags.innerText = value;
+
+//Stockage liste des tags affichés
+localStorage.setItem("tags", listTags.innerText);
+
+
+for (let el of combos) { 
+ 
+//console.log(el.input.value = "");   //????
+               
+      if (el.list == sortedIngredients) {
+        listTags.style.backgroundColor = "#3182f7";
+      }
+      if (el.list == sortedAppareils) {
+        listTags.style.backgroundColor = "#68d9a5";
+      }
+      if (el.list == sortedUstensile) {
+        listTags.style.backgroundColor = "#ed6454";
+      }
+  };
+  //Récup la classe pour condition (el.classlist.contains("nom class"))  => voir includes ou === si prob
+// supp for of pour install contains
+
   let closeTags = document.createElement("img");
   closeTags.src = "img/times-circle-regular.svg";
   closeTags.classList.add("closedtag");
@@ -130,8 +134,39 @@ function displayTags(value) {
   closeTags.addEventListener("click", () => {
     listTags.remove();
   });
-  removeElements();
+
+  removeList();
 }
+
+
+//Fermeture de items (liste ingredients, ustensiles, appareils) ouvert
+function removeList() {
+  let items = document.querySelectorAll(".list-items"); 
+  
+  items.forEach((item) => { 
+    item.remove();
+    
+     initCombo(); 
+  });
+
+}
+
+//Réinitialisation du combo sur le click d'un autre combo
+function initCombo() {
+ combos.forEach((close) => (close.input.value = ""));  
+  for (let combo of combos) { 
+    combo.input.style.borderRadius = "5px";
+    combo.input.addEventListener("click", () => {
+      //combos.forEach((close) => (close.input.value = ""));
+
+      // combo.input.value = "";  //A déplacer au bon endroit
+
+      removeList();
+    });
+  }
+}
+
+//console.log(combo.list = sortedIngredients); //ressort la liste
 
 //arrow = document.querySelector(".toggleArrow");
 // arrow.classList.toggle("toggleArrow")
@@ -140,3 +175,5 @@ function displayTags(value) {
 
 //localStorage.setItem();
 //localStorage.clear();
+
+
--- search.js
+++ search.js
@@ -6,16 +6,61 @@ const searchInput = document.querySelector("#search");
 searchInput.addEventListener("keyup", function () {
   const input = searchInput.value;
 
+array.forEach((recipe) => {
+ 
+  ingredients = recipe.ingredients;
+  let detailsIngredients = ""
+
+  ingredients.forEach((ing) => {
+    ingredient = ing.ingredient;
+    quantity = ing.quantity;
+    unit = ing.unit;
+
+    if (quantity == undefined) {
+      quantity = "";
+    }
+    if (unit == undefined) {
+      unit = "";
+    }
+    
+    const formatUnit = (unit) => {
+    switch (unit) {
+      case "gramme":
+      case "grammes":
+        return "Gr.";
+      case "cuillères à soupe":
+      case "cuillère à soupe":
+        return "C à s.";
+      case "cuillères à café":
+      case "cuillère à café":
+        return "C à c.";
+      case "litres":
+      case "litre":
+        return "L.";
+      default:
+        return unit;
+    }
+  };
+detailsIngredients += `<li><span class="ingredients-details">${ingredient} / </span> ${quantity} ${formatUnit(
+  unit
+)}</li>`;
+  
+
+
+console.log(ingredients);
+})
+
 let suggestion = "";
 
-  array.filter(
-    (item) =>
-      item.name.toLowerCase().includes(input.toLowerCase()) ||
-      item.description.toLowerCase().includes(input.toLowerCase())
-  ).forEach(
-    (r) =>
-   
-      (suggestion += `    <div class="card">
+  array
+    .filter(
+      (item) =>
+        item.name.toLowerCase().includes(input.toLowerCase()) ||
+        item.description.toLowerCase().includes(input.toLowerCase())
+    )
+    .forEach(
+      (r) =>
+        (suggestion += `    <div class="card">
 
               <div class="photosPlats">
               <img src="./img/logo_lespetitsplats.png" class="photoPlat" />
@@ -25,7 +70,7 @@ let suggestion = "";
                 <h3 class="title">${r.name}</h3>
                 <div class="time">
                   <i class="far fa-clock"></i>
-                  <span class="mn">${r.time} mn</span>
+                  <span class="mn">${time} mn</span>
                 </div>
               </div>
 
@@ -33,7 +78,7 @@ let suggestion = "";
                 <div class="ingredients">
                   <ul id="list-ingredients">
                   
-                   <li><span class="ingredients-details">${ingredient}  </span> (${quantity} ${unit})</li>
+                  ${detailsIngredients}
                     </ul>          
                 </div>
                 <div class="préparation">
@@ -42,10 +87,48 @@ let suggestion = "";
               </div>
 
            
-          </div>`
-          )
-  );
+          </div>`)
+    );
   document.querySelector(".containerCards").innerHTML = suggestion;
 });
- 
+ });
+/*
+//          ALGO OPTION 1 BARRE PRINCIPALE
 
+const barreChamp = document.getElementById("barre_champ");
+
+
+function filtreBarre(){
+    const inputBarre = barreChamp.value;
+    let resultat = [];
+
+    if (inputBarre.length >= 3){    //filtre des recettes en relation avec les 3 caractères tapés
+
+        resultat = recettes.filter(recette => recette.name.toLowerCase().includes(inputBarre.toLowerCase())  recette.description.toLowerCase().includes(inputBarre.toLowerCase())  recette.ingredients.some ((ingredient) => ingredient.ingredient.toLowerCase().includes(inputBarre.toLowerCase())));
+
+        recettes = resultat; 
+
+    }else{      //sinon affiche toutes les recettes avec un filtre correspondant aux tags selectionnes
+
+        recettes = recipes;
+        filtreTag();
+        resultat = recettes;
+
+    }
+    if (resultat.length == 0){  //si il n'y a aucune correspondance, affiche un message
+
+        pasDeRecette();
+
+    }else{
+
+        displayRecette(resultat);   //j'affiche le resultat de ce filtre au niveau des recettes
+    }
+
+    displayListe(resultat);     //j'affiche le resultat de ce filtre au niveau des listes btn
+
+}
+
+barreChamp.addEventListener("input", filtreBarre);
+*/
+
+//https://github.com/Christelle74/ChristellePhilippe_7_08032022/blob/master/scripts/index.js
